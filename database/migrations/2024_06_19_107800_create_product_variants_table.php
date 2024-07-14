e827463fd5baa4c38cebbff924aaf56bab650161
Farid Hidayat <e.faridhidayat@gmail.com>
2024-07-14 23:59:13 +0700

    migration: product variants

---
+++ database/migrations/2024_06_19_107800_create_product_variants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('product_id')->references('id')->on('products');
+
+            $table->string('name');
+            $table->enum('type', ['color', 'size', 'tool', 'custom'])->default('custom');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
