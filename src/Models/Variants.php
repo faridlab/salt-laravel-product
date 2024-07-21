ff2075cca9186aa6a4793eeaa2d1b7ea0a295a2c
Farid Hidayat <e.faridhidayat@gmail.com>
2024-07-21 19:19:35 +0700

    feat(model): Variants

---
+++ src/Models/Variants.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SaltProduct\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+use SaltProduct\Traits\Sluggable;
+use SaltProduct\Traits\Orderable;
+
+class Variants extends Resources {
+
+    use Uuids;
+    use ObservableModel;
+    use Sluggable;
+    use Orderable;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'limit',
+        'page',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table categories
+        'id',
+        'name',
+        'type',
+    ];
+
+    protected $rules = array(
+        'name' => 'required|string',
+        'type' => 'nullable|in:color,size,tool,custom',
+    );
+
+    protected $auths = array (
+        // 'index',
+        'store',
+        // 'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $forms = array();
+    protected $structures = array();
+
+    protected $searchable = array(
+        'name',
+        'type',
+    );
+    protected $fillable = array(
+        'name',
+        'type',
+    );
+    protected $casts = [];
+}
