aebddda1f822e542caf6900252ccae66795a9bb3
Farid Hidayat <e.faridhidayat@gmail.com>
2024-07-21 13:11:17 +0700

    refactor(change): fields and relationship to category and brand

--- src/Models/Products.php
+++ src/Models/Products.php
@@ -32,22 +32,56 @@ class Products extends Resources {
         'orderby',
         // Fields table categories
         'id',
-        'parent_id',
-        'type',
-        'type_other',
+        'code',
         'name',
         'slug',
-        'order',
+        'description',
+        'category_id',
+        'currency',
+        'price',
+        'status',
+        'condition',
+        'min_order',
+        'weight_unit',
+        'weight',
+        'dimension_unit',
+        'dimension',
+        'guarantee',
+        'is_must_insurance',
+        'stock',
+        'sku',
+        'sni',
+        'wholesale',
+        'preorder',
+        'variant',
+        'brand_id',
         'data',
     ];
 
     protected $rules = array(
-        'parent_id' => 'nullable|string',
-        'type' => 'required|string',
-        'type_other' => 'nullable|string',
+        'code' => 'nullable|string',
         'name' => 'required|string',
         'slug' => 'nullable|string',
-        'order' => 'nullable|integer',
+        'description' => 'required|string',
+        'category_id' => 'required|string',
+        'currency' => 'nullable|string',
+        'price' => 'required|double',
+        'status' => 'nullable|in:banned,pending,deleted,active,featured,inactive',
+        'condition' => 'nullable|in:new,used',
+        'min_order' => 'nullable|integer',
+        'weight_unit' => 'nullable|in:gram,kg',
+        'weight' => 'nullable|double',
+        'dimension_unit' => 'nullable|in:m,cm,mm',
+        'dimension' => 'nullable|json',
+        'guarantee' => 'nullable|in:distributor,brand,store,indonesia,international,noguarantee',
+        'is_must_insurance' => 'nullable|boolean',
+        'stock' => 'nullable|json',
+        'sku' => 'nullable|string',
+        'sni' => 'nullable|string',
+        'wholesale' => 'nullable|json',
+        'preorder' => 'nullable|json',
+        'variant' => 'nullable|json',
+        'brand_id' => 'nullable|string',
         'data' => 'nullable|json',
     );
 
@@ -70,16 +104,66 @@ class Products extends Resources {
     protected $forms = array();
     protected $structures = array();
 
-    protected $searchable = array('parent_id', 'type', 'type_other', 'name', 'slug', 'order', 'data');
-    protected $fillable = array('parent_id', 'type', 'type_other', 'name', 'slug', 'order', 'data');
+    protected $searchable = array(
+        'code',
+        'name',
+        'slug',
+        'description',
+        'category_id',
+        'currency',
+        'price',
+        'status',
+        'condition',
+        'min_order',
+        'weight_unit',
+        'weight',
+        'dimension_unit',
+        'dimension',
+        'guarantee',
+        'is_must_insurance',
+        'stock',
+        'sku',
+        'sni',
+        'wholesale',
+        'preorder',
+        'variant',
+        'brand_id',
+        'data',
+    );
+    protected $fillable = array(
+        'code',
+        'name',
+        'slug',
+        'description',
+        'category_id',
+        'currency',
+        'price',
+        'status',
+        'condition',
+        'min_order',
+        'weight_unit',
+        'weight',
+        'dimension_unit',
+        'dimension',
+        'guarantee',
+        'is_must_insurance',
+        'stock',
+        'sku',
+        'sni',
+        'wholesale',
+        'preorder',
+        'variant',
+        'brand_id',
+        'data',
+    );
     protected $casts = [];
 
-    public function parent() {
-        return $this->belongsTo('SaltProduct\Models\Categories', 'parent_id', 'id')->withTrashed();
+    public function category() {
+        return $this->belongsTo('SaltCategories\Models\Categories', 'category_id', 'id')->withTrashed();
     }
 
-    public function children() {
-        return $this->hasMany('SaltProduct\Models\Categories', 'parent_id', 'id')->withTrashed();
+    public function brand() {
+        return $this->hasMany('SaltProduct\Models\Brands', 'brand_id', 'id')->withTrashed();
     }
 
 }
