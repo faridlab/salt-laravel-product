6942845f43435da78a224944482366e71c0ffd7f
Farid Hidayat <e.faridhidayat@gmail.com>
2024-07-21 17:04:34 +0700

    feat(model): ProductShowcases

---
+++ src/Models/ProductShowcases.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace SaltProduct\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+use SaltProduct\Traits\Sluggable;
+use SaltProduct\Traits\Orderable;
+
+class ProductShowcases extends Resources {
+
+    use Uuids;
+    use ObservableModel;
+    use Sluggable;
+    use Orderable;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'limit',
+        'page',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table categories
+        'id',
+        'product_id',
+        'showcase_id',
+    ];
+
+    protected $rules = array(
+        'product_id' => 'required|string',
+        'showcase_id' => 'required|string',
+    );
+
+    protected $auths = array (
+        // 'index',
+        'store',
+        // 'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $forms = array();
+    protected $structures = array();
+
+    protected $searchable = array(
+        'product_id',
+        'warehouse_id',
+    );
+    protected $fillable = array(
+        'product_id',
+        'warehouse_id',
+    );
+    protected $casts = [];
+
+    public function porduct() {
+        return $this->hasMany('SaltProduct\Models\Products', 'product_id', 'id')->withTrashed();
+    }
+
+    public function showcase() {
+        return $this->hasMany('SaltProduct\Models\Showcases', 'showcase_id', 'id')->withTrashed();
+    }
+
+}
