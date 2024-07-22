54c88917001c4cffb6c2e7b945eb5b6db82952f4
Farid Hidayat <e.faridhidayat@gmail.com>
2024-07-22 23:01:38 +0700

    feat(model): TrackingStatuses

---
+++ src/Models/TrackingStatuses.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace SaltProduct\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+
+class TrackingStatuses extends Resources {
+
+    use Uuids;
+    use ObservableModel;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'limit',
+        'page',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table brands
+        'id',
+        'order',
+        'group',
+        'status',
+    ];
+
+    protected $rules = array(
+        'order' => 'required|integer',
+        'group' => 'required|string',
+        'status' => 'required|string',
+    );
+
+    protected $auths = array (
+        // 'index',
+        'store',
+        // 'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $forms = array();
+    protected $structures = array();
+    protected $searchable = array(
+        'order',
+        'group',
+        'status',
+    );
+    protected $fillable = array(
+        'order',
+        'group',
+        'status',
+    );
+    protected $casts = [];
+}
