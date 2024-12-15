d88c8aab1ed4fe50d8a7beecec63f7fefb9b2f3e
Farid Hidayat <farid@startapp.id>
2024-12-15 23:08:30 +0700

    refactor: update params validation value

--- src/Traits/ProductCreatable.php
+++ src/Traits/ProductCreatable.php
@@ -34,19 +34,21 @@ trait ProductCreatable
                 $model->preorder = $preorder;
             }
 
-            $stock_total = 100;
-            if(!empty($model->stock) && !is_null($model->stock) && is_numeric($model->stock)) {
-                $stock_total = $model->stock;
-            }
-
             $stock = [
-                "total" => $stock_total,
-                "minimum_alert" => 10,
-                "wording" => "Stock terbatas, buruan dapatkan alat impianmu",
-                "main" => $stock_total,
-                "available" => $stock_total
+                "total" => request()->get('stock_total', 1000),
+                "minimum_alert" => request()->get('stock_minimum_alert', 10),
+                "wording" => request()->get('stock_wording', "Stock terbatas, buruan dapatkan produk impianmu"),
+                "main" => request()->get('stock_total', 1000),
+                "available" => request()->get('stock_total', 1000)
             ];
             $model->stock = $stock;
+
+            $dimension = [
+                "length" => request()->get('dimension_length', 0),
+                "width" => request()->get('dimension_width', 0),
+                "height" => request()->get('dimension_height', 0),
+            ];
+            $model->dimension = $dimension;
 
             if(!is_null($model->price_discount) && !empty($model->price_discount)) {
                 $discount = ((float) $model->price_discount / (float) $model->price) * 100;
@@ -55,10 +57,34 @@ trait ProductCreatable
         });
 
         static::updating(function ($model) {
+            if (request()->isMethod('patch')) {
+                $params = request()->all();
+                foreach ($params as $key => $value) {
+                    $model[$key] = $value;
+                }
+                return;
+            }
+
             if(!is_null($model->price_discount) && !empty($model->price_discount)) {
                 $discount = ((float) $model->price_discount / (float) $model->price) * 100;
                 $model->price_discount_percentage = (float) number_format((float) $discount, 1, '.', '');
             }
+
+            $stock = [
+                "total" => request()->get('stock_total', $model->stock->total),
+                "minimum_alert" => request()->get('stock_minimum_alert', $model->stock->minimum_alert),
+                "wording" => request()->get('stock_wording', $model->stock->wording),
+                "main" => request()->get('stock_total', $model->stock->main),
+                "available" => request()->get('stock_total', $model->stock->available)
+            ];
+            $model->stock = $stock;
+
+            $dimension = [
+                "length" => request()->get('dimension_length', $model->dimension->length),
+                "width" => request()->get('dimension_width', $model->dimension->width),
+                "height" => request()->get('dimension_height', $model->dimension->height),
+            ];
+            $model->dimension = $dimension;
 
             $name = $model->name;
             $old_name = $model->getOriginal('name');
